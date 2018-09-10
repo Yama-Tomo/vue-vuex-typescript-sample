a2e5c984ba62218921591594df5fb349fb007a0b
yamaya_tomohiro <yamaya.tomohiro@adways.net>
2018-09-10 17:42:19 +0900

    feat: added layouts.

---
+++ src/layouts/error.vue
@@ -0,0 +1,21 @@
+<template>
+  <div>
+    <h1 v-if="error.statusCode === 404">Sorry Not Found...</h1>
+    <h1 v-else>Internal Server Error</h1>
+    <nuxt-link to="/">Home</nuxt-link>
+  </div>
+</template>
+
+<script lang="ts">
+import Component from 'nuxt-class-component';
+import { Prop, Vue } from 'vue-property-decorator';
+import { Nuxt } from '../types';
+
+@Component
+export default class About extends Vue {
+  @Prop(Object)
+  public error!: Nuxt.Error;
+
+  get statusCode(): number { return this.error.statusCode; }
+}
+</script>
