aa1ff2e71626457512447f2c93f83985e8ca4302
yamaya_tomohiro <yamaya.tomohiro@adways.net>
2018-09-10 17:41:53 +0900

    feat: nuxt error struct.

--- src/types.ts
+++ src/types.ts
@@ -1,3 +1,12 @@
 export interface HTMLElementEvent<T extends HTMLElement> extends Event {
   target: T;
 }
+
+/* tslint:disable:no-namespace */
+export namespace Nuxt {
+  export interface Error {
+    statusCode: number;
+    path: string;
+    message: string;
+  }
+}
