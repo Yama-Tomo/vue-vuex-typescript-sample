e26c052181dd80bd6955ab7209c1e7018c628bb7
Yama-Tomo <mazda13bturbo@gmail.com>
2018-09-27 12:56:44 +0900

    feat: routing

--- __api/config/routes.rb
+++ __api/config/routes.rb
@@ -1,3 +1,5 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  scope :api, defaults: {format: :json} do
+    devise_for :users, controllers: {sessions: 'sessions'}
+  end
 end
