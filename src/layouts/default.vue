5dd0419154de158c261cee8bad8525ecf59db8d6
Yama-Tomo <mazda13bturbo@gmail.com>
2018-09-28 21:25:15 +0900

    feat: show login link if not sign in

--- src/layouts/default.vue
+++ src/layouts/default.vue
@@ -1,9 +1,27 @@
 <template>
   <div>
     <header class="header">Vuex tutorial with typescript and Nuxt.js</header>
+    <div class="signin">
+      <nuxt-link to="/login" v-if="!isLoggedIn">sign in</nuxt-link>
+    </div>
     <nuxt class="container" />
   </div>
 </template>
+
+<script lang="ts">
+import Component from 'nuxt-class-component';
+import { Mixins } from 'vue-mixin-decorator';
+import { StoreHelperMixin } from '../mixins/store_helper';
+import * as ns from '../namespace_maps';
+import { AuthState } from '../modules/auth/store/state';
+
+@Component
+export default class DefaultLayout extends Mixins<StoreHelperMixin>(StoreHelperMixin) {
+  get isLoggedIn(): boolean {
+    return (this.getState(ns.authModuleName) as AuthState).loggedIn;
+  }
+}
+</script>
 
 <style scoped lang="scss">
 @import "../assets/css/variables";
@@ -18,5 +36,9 @@
 
 .container {
   margin: 15px;
+}
+
+.signin {
+  text-align: right;
 }
 </style>
