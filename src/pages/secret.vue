b223a11fe9a9ba0672162fe7aae89c84cc093979
Yama-Tomo <mazda13bturbo@gmail.com>
2018-09-28 23:21:05 +0900

    feat: added secret page

---
+++ src/pages/secret.vue
@@ -0,0 +1,25 @@
+<template>
+  <div>
+    <h1>Secret</h1>
+    This is secret page.
+    <br>
+    <br>
+    your email address
+    {{ email }}
+  </div>
+</template>
+
+<script lang="ts">
+import Component from 'nuxt-class-component';
+import { StoreHelperMixin } from '../mixins/store_helper';
+import { Mixins } from 'vue-mixin-decorator';
+import * as ns from '../namespace_maps';
+import { AuthState } from '../modules/auth/store/state';
+
+@Component
+export default class About extends Mixins<StoreHelperMixin>(StoreHelperMixin) {
+  get email(): string {
+    return (this.getState(ns.authModuleName) as AuthState).user.email;
+  }
+}
+</script>
