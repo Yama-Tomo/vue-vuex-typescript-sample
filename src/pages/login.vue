c47fbdb685f5dfcfa30bedeb7a43f83cd70ce9d9
Yama-Tomo <mazda13bturbo@gmail.com>
2018-09-28 21:14:20 +0900

    feat: added login page

---
+++ src/pages/login.vue
@@ -0,0 +1,46 @@
+<template>
+  <div>
+    <h1>login</h1>
+    <div>
+      <label>User Name</label><br>
+      <input type="text" v-model="username">
+    </div>
+    <div>
+      <label>Password</label><br>
+      <input type="password" v-model="password">
+    </div>
+    <button @click="onLoginButtonClick">login</button>
+    <hr />
+    <nuxt-link to="/">Home</nuxt-link>
+  </div>
+</template>
+
+<script lang="ts">
+import Component from 'nuxt-class-component';
+import { Vue } from 'vue-property-decorator';
+
+@Component
+export default class Login extends Vue {
+  public username = '';
+  public password = '';
+
+  public onLoginButtonClick() {
+    this.$auth.loginWith('local', { data: {
+      user: {
+        email: this.username,
+        password: this.password,
+      },
+    }});
+  }
+}
+</script>
+
+<style scoped lang="scss">
+button {
+  margin-top: 20px;
+  margin-bottom: 20px;
+  font-size: 15px;
+  width: 110px;
+  border-radius: 5px;
+}
+</style>
