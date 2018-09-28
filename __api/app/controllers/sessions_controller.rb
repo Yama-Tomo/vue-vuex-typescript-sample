6c831bff6931ab1a835ec51feae3480f8cd6feac
Yama-Tomo <mazda13bturbo@gmail.com>
2018-09-28 21:24:13 +0900

    feat: user info api

--- __api/app/controllers/sessions_controller.rb
+++ __api/app/controllers/sessions_controller.rb
@@ -1,3 +1,16 @@
 class SessionsController < Devise::SessionsController
   respond_to :json
+
+  before_action :verify_authentication, only: [:show]
+
+  def show
+    render json: { :user => { id: current_user.id, email: current_user.email } }
+  end
+
+  def verify_authentication
+    unless user_signed_in?
+      render json: { error: 'forbidden' }, status: 403
+      return false
+    end
+  end
 end
--- __api/config/routes.rb
+++ __api/config/routes.rb
@@ -1,5 +1,8 @@
 Rails.application.routes.draw do
   scope :api, defaults: {format: :json} do
     devise_for :users, controllers: {sessions: 'sessions'}
+    devise_scope :user do
+      get 'users/show', to: 'sessions#show'
+    end
   end
 end
