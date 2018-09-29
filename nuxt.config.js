d347d1ffca86cd2a29359af52c0b6efc4ddbb42c
Yama-Tomo <mazda13bturbo@gmail.com>
2018-09-29 17:24:31 +0900

    Revert "chore: removed unnecessary webpack build setting"
    This reverts commit 6d2f16549ffae8ba6d8dea965a8f11f64f8b94c1.

--- nuxt.config.js
+++ nuxt.config.js
@@ -49,6 +49,9 @@ module.exports = {
   css: ['~/assets/css/main.scss'],
   build: {
     extractCSS: { allChunks: true },
+    stats: {
+      warningsFilter: /export .* was not found in/,
+    }
   },
   modules: [
     '~/nuxt_with_typescript.js',
