51e7d4d471ef3c19483e007318dbb673957f98bd
Yama-Tomo <mazda13bturbo@gmail.com>
2018-12-04 17:25:47 +0900

    chore: fix typo

--- src/index.d.ts
+++ src/index.d.ts
@@ -1,9 +1,9 @@
 import Vue, { ComponentOptions } from 'vue';
 import { Route } from 'vue-router';
-import { Store } from 'vuex'
-import { NuxtAxiosInstance } from '@nuxtjs/axios'
-import VueI18n from 'vue-i18n'
-import * as NuxtI18nTypes from 'nuxt-i18n/types'
+import { Store } from 'vuex';
+import { NuxtAxiosInstance } from '@nuxtjs/axios';
+import VueI18n from 'vue-i18n';
+import * as NuxtI18nTypes from 'nuxt-i18n/types';
 
 declare module 'vue/types/options' {
   interface ComponentOptions<V extends Vue> {
@@ -12,34 +12,34 @@ declare module 'vue/types/options' {
 }
 
 interface NuxtContext {
-  isDev: boolean
-  isHMR: boolean
-  isStatic: boolean
+  isDev: boolean;
+  isHMR: boolean;
+  isStatic: boolean;
   route: Route;
-  store: Store<any>
-  env: object
-  query: { [key: string]: any }
-  nuxtState: object
-  req: Request
-  res: Response
-  params: { [key: string]: any }
-  redirect: (path: string) => void
-  error: (params: { statusCode?: Number; message?: String }) => void
-  beforeNuxtRender: (arg: { Conmponents: any, nuxtState: any }) => void
-  $axios: NuxtAxiosInstance  // axios-moduleを利用している場合
+  store: Store<any>;
+  env: object;
+  query: { [key: string]: any };
+  nuxtState: object;
+  req: Request;
+  res: Response;
+  params: { [key: string]: any };
+  redirect: (path: string) => void;
+  error: (params: { statusCode?: number; message?: string }) => void;
+  beforeNuxtRender: (arg: { Conmponents: any, nuxtState: any }) => void;
+  $axios: NuxtAxiosInstance;  // axios-moduleを利用している場合
   app: {
     $auth: {
       ctx: NuxtContext
       options: { [key: string]: any }
       strategies: { [key: string]: any }
-      redirect: (name: string, noRouter?: boolean) => void
+      redirect: (name: string, noRouter?: boolean) => void,
       // TODO: _errorListeners, $storage, $stateの定義
     }
     i18n: VueI18n
+    store: Store<any>
     localePath(route: string | Route, locale?: string): string
     switchLocalePath(locale: string): string
-    getRouteBaseName(route: Route): string
-    store: Store<any>
+    getRouteBaseName(route: Route): string,
   };
 }
 
@@ -51,10 +51,10 @@ declare module 'vue/types/vue' {
       strategies: { [key: string]: any }
       redirect: (name: string, noRouter?: boolean) => void
       loginWith: (strategy: string, params: any) => Promise<any>
-      logout: () => Promise<any>
-    }
-    $axios: NuxtAxiosInstance
-    $route: Route
+      logout: () => Promise<any>,
+    };
+    $axios: NuxtAxiosInstance;
+    $route: Route;
   }
 }
 
