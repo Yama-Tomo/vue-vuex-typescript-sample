58fa5651fc7b6a9c22b765daa7f81770df8b2994
Yama-Tomo <mazda13bturbo@gmail.com>
2019-02-12 21:44:43 +0900

    refactor: storeの生成を関数化

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,11 @@
 import Vue from 'vue';
 import App from './App.vue';
 import store from './store';
+import { modules } from './namespace_maps';
 
 Vue.config.productionTip = false;
 
 new Vue({
-  store,
+  store: store(modules.todo),
   render: (h) => h(App),
 }).$mount('#app');
--- src/store.ts
+++ src/store.ts
@@ -1,12 +1,20 @@
 import Vue from 'vue';
 import Vuex from 'vuex';
 import { modules } from './namespace_maps';
-import { todoStore } from './modules/todo/store';
+import todoStore from './modules/todo/store';
 
 Vue.use(Vuex);
 
-export default new Vuex.Store({
-  modules: {
-    [modules.todo]: todoStore,
-  },
-});
+export default (moduleName: string) => {
+  const storeModules = (() => {
+    if (moduleName === modules.todo) {
+      return { [modules.todo]: todoStore() };
+    }
+
+    throw new Error('invalid module name');
+  })();
+
+  return new Vuex.Store({
+    modules: storeModules,
+  });
+};
