ab648dfa91a0453f2a9cb6dccee25ba34c8722e6
Yama-Tomo <mazda13bturbo@gmail.com>
2019-02-12 16:56:25 +0900

    feat: webpackを削除してvue-cli-serviceにビルド設定を移行

---
+++ vue.config.js
@@ -0,0 +1,25 @@
+const path = require('path');
+
+class IgnoreNotFoundExportPlugin {
+  apply(compiler) {
+    compiler.hooks.done.tap('warnfix-plugin', (stats) => {
+      const messageRegExp = /export .* was not found in/
+      stats.compilation.warnings = stats.compilation.warnings.filter((warn) => {
+        if (warn.name === 'ModuleDependencyWarning' && messageRegExp.test(warn.message)) {
+          return false
+        }
+        return true
+      })
+    })
+  }
+}
+
+module.exports = {
+  configureWebpack: config => {
+    config.entry = './src/index.ts';
+    config.output.filename = 'index.js';
+    config.output.path = path.resolve(__dirname, './dist');
+
+    config.plugins.unshift(new IgnoreNotFoundExportPlugin())
+  }
+}
