d1ee9c8be116287efce031841df56db5e65824e3
Yama-Tomo <mazda13bturbo@gmail.com>
2019-02-13 13:26:24 +0900

    fix: removed unnecessary files

--- src/modules/global/store/getters.ts
+++
@@ -1,6 +0,0 @@
-import { DefineGetters } from 'vuex-type-helper';
-import { GlobalState } from './state';
-
-export interface GlobalGetters {}
-
-export const getters: DefineGetters<GlobalGetters, GlobalState> = {};
--- src/modules/global/store/state.ts
+++
@@ -1,7 +0,0 @@
-export interface GlobalState {
-  hiddenLayout: boolean;
-}
-
-export const state: GlobalState = {
-  hiddenLayout: false,
-};
