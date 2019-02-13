bec374eb47d9c5a8e04f9a19f3fc34fb076a78d5
Yama-Tomo <mazda13bturbo@gmail.com>
2019-02-13 14:35:30 +0900

    refactor: migrate to module mode of nuxt store

--- src/store/index.ts
+++
@@ -1,8 +0,0 @@
-import Vuex from 'vuex';
-import { stores } from '../namespace_maps';
-
-export default function store() {
-  return new Vuex.Store({
-    modules: stores,
-  });
-}
---
+++ src/store/todo.ts
@@ -0,0 +1,5 @@
+import state from '@/store_modules/todo/state';
+import actions from '@/store_modules/todo/actions';
+import mutations from '@/store_modules/todo/mutations';
+
+export { state, actions, mutations };
