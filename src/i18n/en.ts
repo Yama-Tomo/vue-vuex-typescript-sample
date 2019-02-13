99cd4dbfb1b280e06e70832405e5c37e92975f64
Yama-Tomo <mazda13bturbo@gmail.com>
2019-02-13 23:34:12 +0900

    feat: added entiry point and dictionary files for i18n

---
+++ src/i18n/en.ts
@@ -0,0 +1,9 @@
+/* tslint:disable:no-var-requires */
+import merge from 'lodash/merge';
+
+const dictionary = merge(
+  require('~/i18n/yml/en.yml'),
+  require('~/i18n/yml/todo/en.yml'),
+);
+
+export default dictionary;
---
+++ src/i18n/ja.ts
@@ -0,0 +1,9 @@
+/* tslint:disable:no-var-requires */
+import merge from 'lodash/merge';
+
+const dictionary = merge(
+  require('~/i18n/yml/ja.yml'),
+  require('~/i18n/yml/todo/ja.yml'),
+);
+
+export default dictionary;
