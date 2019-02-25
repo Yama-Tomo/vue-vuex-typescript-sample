df57e53b53746d59330e1e15b1cd12c73a5ae9c5
Yama-Tomo <mazda13bturbo@gmail.com>
2019-02-25 14:59:03 +0900

    chore: minor changes

--- extend.webpack.config.js
+++ extend.webpack.config.js
@@ -8,9 +8,9 @@ class IgnoreNotFoundExportPlugin {
       const messageRegExp = /export .* was not found in/
       stats.compilation.warnings = stats.compilation.warnings.filter((warn) => {
         if (warn.name === 'ModuleDependencyWarning' && messageRegExp.test(warn.message)) {
-          return false;
+          return false
         }
-        return true;
+        return true
       })
     })
   }
@@ -20,37 +20,37 @@ module.exports = function(config, ctx) {
   const useVueI18nLoader = (() => {
     const nuxtI18n = this.context.options.modules.find((module) => {
       if (Array.isArray(module) ) {
-        return module[0] === 'nuxt-i18n';
+        return module[0] === 'nuxt-i18n'
       }
-      return false;
-    });
+      return false
+    })
 
-    return nuxtI18n !== undefined && !!nuxtI18n[1].vueI18nLoader;
-  })();
+    return nuxtI18n !== undefined && !!nuxtI18n[1].vueI18nLoader
+  })()
 
   if (useVueI18nLoader) {
     config.module.rules.push({
       resourceQuery: /blockType=i18n/,
       type: 'javascript/auto',
       loader: ['@kazupon/vue-i18n-loader', 'yaml-loader']
-    });
+    })
   } else {
     config.module.rules.push({
       test: /\.ya?ml$/,
       loaders: ['json-loader', 'yaml-loader']
-    });
+    })
   }
 
-  config.plugins.push(new IgnoreNotFoundExportPlugin());
+  config.plugins.push(new IgnoreNotFoundExportPlugin())
 
   config.plugins.push(new HardSourceWebpackPlugin({
     cacheDirectory: __dirname + '/node_modules/.cache/hard-source/[confighash]',
     configHash: function(webpackConfig) {
-      return defaultConfigHash(webpackConfig) + '-' + process.env.NODE_ENV;
+      return defaultConfigHash(webpackConfig) + '-' + process.env.NODE_ENV
     },
-  }));
+  }))
 
   config.plugins.push(new HardSourceWebpackPlugin.ExcludeModulePlugin([
     { test: /extract-css-chunks-webpack-plugin[\\/]dist[\\/]hotLoader/ }
-  ]));
-};
+  ]))
+}
