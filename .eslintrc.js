94d41fc8fbe1c25af07b7fa1fd867d9f773ef6ba
Yama-Tomo <mazda13bturbo@gmail.com>
2019-03-26 10:21:31 +0900

    feat: added lint settings

---
+++ .eslintrc.js
@@ -0,0 +1,20 @@
+// NOTE: how to apply eslint on intellij https://intellij-support.jetbrains.com/hc/en-us/community/posts/115000225170-ESLint-and-ts-Typescript-files
+module.exports = {
+  root: true,
+  plugins: ['@typescript-eslint', 'babel'],
+  parserOptions: {
+    parser: '@typescript-eslint/parser',
+    sourceType: 'module',
+  },
+  rules: {
+    semi: 'off',
+    'babel/semi': 'error',
+    'comma-dangle': ['error', 'always-multiline'],
+    '@typescript-eslint/no-unused-vars': 'error',
+    '@typescript-eslint/member-delimiter-style': 'error',
+    '@typescript-eslint/explicit-member-accessibility': 'error',
+  },
+  extends: [
+    '@nuxtjs',
+  ]
+}
