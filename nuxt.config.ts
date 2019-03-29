a3cb56122b28fa6ac28a75b50b401acf66736fba
Yama-Tomo <mazda13bturbo@gmail.com>
2019-03-29 13:53:53 +0900

    feat: disable nuxt-i18n detect browser lang feature

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -69,9 +69,7 @@ export default {
       ],
       langDir: 'i18n/',
       strategy: 'prefix',
-      detectBrowserLanguage: {
-        useCookie: true,
-      },
+      detectBrowserLanguage: false,
     }],
   ],
   plugins: [
