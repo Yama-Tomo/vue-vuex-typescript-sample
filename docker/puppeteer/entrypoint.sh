3c4825db1eaa3bbdce673063398e1d13349a0c61
Yama-Tomo <mazda13bturbo@gmail.com>
2019-03-31 01:23:45 +0900

    refactor: improve docker image size (puppeteer

--- docker/puppeteer/entrypoint.sh
+++ docker/puppeteer/entrypoint.sh
@@ -1,11 +1,9 @@
-#!/bin/bash
+#!/bin/sh
 
 USER_ID=${LOCAL_UID:-9001}
 GROUP_ID=${LOCAL_GID:-9001}
 
 echo "Starting with UID : $USER_ID, GID: $GROUP_ID"
-useradd -u $USER_ID -o -m user
-groupmod -g $GROUP_ID user
-export HOME=/home/user
+adduser -u $USER_ID -g $GROUP_ID -s /bin/sh -D user
 
-exec /usr/sbin/gosu user "$@"
+exec su-exec user "$@"
--- docker/puppeteer/wait_for_backend.sh
+++ docker/puppeteer/wait_for_backend.sh
@@ -1,6 +1,6 @@
-#!/bin/bash
+#!/bin/sh
 
-until curl $BACKEND_HOST >/dev/null 2>&1; do
+until wget -O - $BACKEND_HOST >/dev/null 2>&1; do
   >&2 echo "Wait for backend API server"
   sleep 1
 done
