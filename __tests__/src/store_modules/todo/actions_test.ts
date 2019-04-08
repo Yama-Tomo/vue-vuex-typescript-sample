5e52b43fc465db901aa62bc07c1bd9e920435be4
Yama-Tomo <mazda13bturbo@gmail.com>
2019-04-08 18:20:17 +0900

    test: added vuex action tests

---
+++ __tests__/src/store_modules/todo/actions_test.ts
@@ -0,0 +1,26 @@
+/* eslint-disable import/first */
+jest.mock('@/store_modules/todo/state', () => ({
+  __esModule: true,
+  ...jest.requireActual('@/store_modules/todo/state'),
+  initialStateResolver: jest.fn(),
+}));
+
+import actions, { TodoActions } from '@/store_modules/todo/actions';
+import { TodoMutations } from '@/store_modules/todo/mutations';
+import { TodoState, initialStateResolver } from '@/store_modules/todo/state';
+import actionContextHelper from '../../../utils/action_context_helper';
+
+afterEach(() => jest.resetAllMocks());
+
+describe('fetchInitialState', () => {
+  test('called commit methods correctly', async () => {
+    const { context, commit } = actionContextHelper<TodoState, {}, TodoActions, TodoMutations>();
+    const mockData = { dummy: 'test' };
+    (initialStateResolver as any as jest.Mock).mockReturnValue(mockData);
+
+    await actions.fetchInitialState(context, undefined);
+
+    expect(commit.mock.calls[0]).toEqual(['resetState', undefined]);
+    expect(commit.mock.calls[1]).toEqual(['setInitialState', mockData]);
+  });
+});
---
+++ __tests__/utils/action_context_helper.ts
@@ -0,0 +1,12 @@
+import { ActionContext } from 'vuex-type-helper';
+
+// eslint-disable-next-line arrow-parens
+export default <T1, T2, T3, T4>(): { context: ActionContext<T1, T2, T3, T4>; commit: jest.Mock<any, any> } => {
+  const commit = jest.fn();
+
+  const context = ({
+    commit: commit,
+  }) as any as ActionContext<T1, T2, T3, T4>;
+
+  return { context, commit };
+};
