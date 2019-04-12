c7ff3e3c34c3385854c7fe34307ce88aa012e54a
Yamatomo <mazda13bturbo@gmail.com>
2019-04-12 22:43:03 +0900

    feat: added bind to function call statement, because so that require `vuex context`

--- __tests__/src/store_modules/todo/actions_test.ts
+++ __tests__/src/store_modules/todo/actions_test.ts
@@ -9,16 +9,18 @@ import actions, { TodoActions } from '@/store_modules/todo/actions';
 import { TodoMutations } from '@/store_modules/todo/mutations';
 import { TodoState, initialStateResolver } from '@/store_modules/todo/state';
 import actionContextHelper from '../../../utils/action_context_helper';
+import { Store } from 'vuex';
 
 afterEach(() => jest.resetAllMocks());
 
 describe('fetchInitialState', () => {
   test('called commit methods correctly', async () => {
+    const store = {} as Store<TodoState>;
     const { context, commit } = actionContextHelper<TodoState, {}, TodoActions, TodoMutations>();
     const mockData = { dummy: 'test' };
     (initialStateResolver as any as jest.Mock).mockReturnValue(mockData);
 
-    await actions.fetchInitialState(context, undefined);
+    await actions.fetchInitialState.bind(store)(context, undefined);
 
     expect(commit.mock.calls[0]).toEqual(['resetState', undefined]);
     expect(commit.mock.calls[1]).toEqual(['setInitialState', mockData]);
