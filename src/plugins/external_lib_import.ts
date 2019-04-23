bea953c6e3d5c0c04f238cd7fd45949b755149b6
Yama-Tomo <mazda13bturbo@gmail.com>
2019-04-23 11:25:17 +0900

    feat: added a plugin that put the loaded instance in the library container

---
+++ src/plugins/external_lib_import.ts
@@ -0,0 +1,30 @@
+import { Nuxt } from '@/types/nuxt';
+import ExternalLibWrapper from '@/utils/external_lib_wrapper';
+
+const sleep = () => new Promise(resolve => setTimeout(resolve, 500));
+
+const waitLoadedExternalLib = async (assertFunction: () => boolean) => {
+  if (assertFunction()) {
+    return;
+  }
+
+  await sleep();
+  await waitLoadedExternalLib(assertFunction);
+};
+
+export default (
+  ctx: Nuxt.Context,
+  inject: (
+    pluginName: string,
+    f: (...args: unknown[]) => unknown | void
+  ) => void
+) => {
+  inject('importJQuery', async () => {
+    await waitLoadedExternalLib(() => (window as any).jQuery !== undefined);
+
+    try {
+      const module = await import('jquery');
+      ExternalLibWrapper.jquery = module.default;
+    } catch (e) {}
+  });
+};
