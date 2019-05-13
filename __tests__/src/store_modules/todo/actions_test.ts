9313a5419526c1e8075039b108b930ba947c8064
Yamatomo <mazda13bturbo@gmail.com>
2019-05-14 00:42:36 +0900

    test: add action test

--- __tests__/src/store_modules/todo/actions_test.ts
+++ __tests__/src/store_modules/todo/actions_test.ts
@@ -11,17 +11,18 @@ import { TodoState, initialStateResolver } from '@/store_modules/todo/state';
 import actionContextHelper from '../../../utils/action_context_helper';
 import { Store } from 'vuex';
 
+const store = {} as Store<TodoState>;
+const { context, commit } = actionContextHelper<
+  TodoState,
+  {},
+  TodoActions,
+  TodoMutations
+>();
+
 afterEach(() => jest.resetAllMocks());
 
 describe('fetchInitialState', () => {
   test('called commit methods correctly', async () => {
-    const store = {} as Store<TodoState>;
-    const { context, commit } = actionContextHelper<
-      TodoState,
-      {},
-      TodoActions,
-      TodoMutations
-    >();
     const mockData = { dummy: 'test' };
     ((initialStateResolver as any) as jest.Mock).mockReturnValue(mockData);
 
@@ -29,5 +30,15 @@ describe('fetchInitialState', () => {
 
     expect(commit.mock.calls[0]).toEqual(['resetState', undefined]);
     expect(commit.mock.calls[1]).toEqual(['setInitialState', mockData]);
+  });
+});
+
+describe('addTodo', () => {
+  test('called commit methods correctly', () => {
+    actions.addTodo.bind(store)(context, { text: 'aaa' });
+    expect(commit.mock.calls[0]).toEqual([
+      'addTodo',
+      { text: 'aaa', done: false },
+    ]);
   });
 });
