3439c4bbc29b083c2e6cf6ed608b5e0fefd89f88
Yama-Tomo <mazda13bturbo@gmail.com>
2019-05-14 17:21:53 +0900

    test(refactor): action context helper

--- __tests__/utils/action_context_helper.ts
+++ __tests__/utils/action_context_helper.ts
@@ -1,7 +1,9 @@
 import { ActionContext } from 'vuex-type-helper';
 
 // eslint-disable-next-line arrow-parens
-export default <T1, T2, T3, T4>(): {
+export default <T1, T2, T3, T4>(
+  state: T1
+): {
   context: ActionContext<T1, T2, T3, T4>;
   commit: jest.Mock<any, any>;
 } => {
@@ -9,6 +11,7 @@ export default <T1, T2, T3, T4>(): {
 
   const context = ({
     commit: commit,
+    state,
   } as any) as ActionContext<T1, T2, T3, T4>;
 
   return { context, commit };
