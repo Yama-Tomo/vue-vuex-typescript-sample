a443e67634c427bf0b6b1b965b2dd9d0807617b5
Yama-Tomo <mazda13bturbo@gmail.com>
2019-05-14 17:22:13 +0900

    test: add action test

--- __tests__/src/store_modules/todo/actions_test.ts
+++ __tests__/src/store_modules/todo/actions_test.ts
@@ -42,3 +42,68 @@ describe('addTodo', () => {
     ]);
   });
 });
+
+describe('removeTodo', () => {
+  test('called commit methods correctly', () => {
+    const todo = { text: 'test', done: true };
+    actions.removeTodo.bind(store)(context, { todo });
+
+    expect(commit.mock.calls[0]).toEqual(['removeTodo', todo]);
+  });
+});
+
+describe('editTodo', () => {
+  test('called commit methods correctly', () => {
+    const todo = { text: 'test', done: true };
+    const text = 'change text';
+    actions.editTodo.bind(store)(context, { todo, text });
+
+    expect(commit.mock.calls[0]).toEqual(['updateTodo', { todo, text }]);
+  });
+});
+
+describe('toggleTodo', () => {
+  test('called commit methods correctly', () => {
+    const todo = { text: 'test', done: true };
+    actions.toggleTodo.bind(store)(context, { todo });
+
+    expect(commit.mock.calls[0]).toEqual(['updateTodo', { todo, done: false }]);
+  });
+});
+
+describe('toggleAll', () => {
+  test('called commit methods correctly', () => {
+    const todo1 = { text: 'test', done: true };
+    const todo2 = { text: 'test2', done: false };
+    const state: TodoState = { todos: [todo1, todo2] };
+
+    actions.toggleAll.bind(store)(
+      { ...context, ...{ state } },
+      { done: false }
+    );
+
+    expect(commit.mock.calls[0]).toEqual([
+      'updateTodo',
+      { todo: todo1, done: false },
+    ]);
+
+    expect(commit.mock.calls[1]).toEqual([
+      'updateTodo',
+      { todo: todo2, done: false },
+    ]);
+  });
+});
+
+describe('clearCompleted', () => {
+  test('called commit methods correctly', () => {
+    const todo1 = { text: 'test', done: true };
+    const todo2 = { text: 'test2', done: false };
+    const todo3 = { text: 'test3', done: true };
+    const state: TodoState = { todos: [todo1, todo2, todo3] };
+
+    actions.clearCompleted.bind(store)({ ...context, ...{ state } }, {});
+
+    expect(commit.mock.calls[0]).toEqual(['removeTodo', todo1]);
+    expect(commit.mock.calls[1]).toEqual(['removeTodo', todo3]);
+  });
+});
