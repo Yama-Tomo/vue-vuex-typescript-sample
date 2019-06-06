54a8b4eeb3a7df9368aabb9118055b06851a2a52
Yama-Tomo <mazda13bturbo@gmail.com>
2019-06-06 09:56:23 +0900

    refactor: merge cookies of the previous request and the last request except for the same key

--- src/plugins/axios_cookie_proxy.ts
+++ src/plugins/axios_cookie_proxy.ts
@@ -1,4 +1,5 @@
 import { Nuxt } from '@/types/nuxt';
+import isNum from 'lodash/isNumber';
 
 export default (ctx: Nuxt.Context) => {
   if (!process.server) {
@@ -20,6 +21,29 @@ export default (ctx: Nuxt.Context) => {
 
     if (Array.isArray(cookie) && cookie.length) {
       // NOTE: バックエンドに通信した際に送られるcookieをそのままSSRのレスポンスにも乗っける
+      const currentCookie = ctx.res.getHeader('Set-Cookie');
+
+      if (currentCookie) {
+        const castCurrentCookie = Array.isArray(currentCookie)
+          ? currentCookie
+          : isNum(currentCookie)
+          ? [String(currentCookie)]
+          : [currentCookie];
+
+        (castCurrentCookie as (string | undefined)[])
+          .filter(v => v)
+          .forEach(v => {
+            if (!v) {
+              return;
+            }
+
+            const [key] = v.split('=');
+            if (cookie.find(v => v.indexOf(key) < 0)) {
+              cookie.push(v);
+            }
+          });
+      }
+
       ctx.res.setHeader('Set-Cookie', cookie);
       // NOTE: 複数回，axiosを使った通信に対応するため送られてきたcookieをデフォルトのヘッダーに代入
       ctx.$axios.defaults.headers.common.cookie = cookie.join('; ');
