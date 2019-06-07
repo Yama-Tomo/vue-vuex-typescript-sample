cfb78fbaa82b88b8ccb8362cd28d4e46c9631164
Yama-Tomo <mazda13bturbo@gmail.com>
2019-06-07 16:09:47 +0900

    fix: fixup slice index

--- src/store_modules/todo/getters.ts
+++ src/store_modules/todo/getters.ts
@@ -10,7 +10,7 @@ export interface TodoGetters {
 export const getters: DefineGetters<TodoGetters, TodoState> = {
   reverse: state => [...state.todos].reverse(),
   latest: (state, that) => limit => {
-    return that.reverse.slice(0, limit - 1);
+    return that.reverse.slice(0, limit);
   },
 };
 
