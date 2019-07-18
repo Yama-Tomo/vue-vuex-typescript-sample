475923bf5c4a9682a314cc94801d31d484cf40fd
Yama-Tomo <mazda13bturbo@gmail.com>
2019-07-18 15:29:37 +0900

    refactor: split vue file

---
+++ src/components/todo/list/functions.ts
@@ -0,0 +1,12 @@
+export const pluralize = (wordLength: number, word: string) =>
+  wordLength === 1 ? word : word + 's';
+
+export const objectToArray = <T extends Object>(obj: T) => {
+  const array: Array<{ key: keyof T; value: T[keyof T] }> = [];
+
+  for (const key of Object.keys(obj) as Array<keyof T>) {
+    array.push({ key, value: obj[key] });
+  }
+
+  return array;
+};
