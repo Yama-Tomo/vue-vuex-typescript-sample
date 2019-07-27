568a1b3c6ff1aa0191b1b8075ccf748fcc541d97
Yamatomo <mazda13bturbo@gmail.com>
2019-07-27 14:09:54 +0900

    chore: minor change

--- stories/components/todo/item.story.ts
+++ stories/components/todo/item.story.ts
@@ -3,17 +3,22 @@ import * as Helper from '../../helper';
 import Item from '@/components/todo/item.vue';
 import { StoreHelper } from '@/mixins/store_helper';
 import * as Mapper from '@/store_modules/module_mapper';
+import { TodoState } from '@/store_modules/todo';
 
 storiesOf('components.todo.item', module).add('default', () => {
   const store = Helper.store();
+  const initialState: TodoState = { todos: [{ text: 'aaaa', done: true }] };
+  store.commit('todo/setInitialState', initialState);
+
   const actions = StoreHelper.getActions(store, Mapper.modules.todo);
+  const state = StoreHelper.getState(store, Mapper.modules.todo);
 
   return {
     components: { Item },
     template: '<Item :actions=actions :todo=todo />',
     store,
     data: () => ({
-      todo: { text: 'aaaa', done: true },
+      todo: state.todos[0],
       actions,
     }),
     i18n: Helper.i18n(),
