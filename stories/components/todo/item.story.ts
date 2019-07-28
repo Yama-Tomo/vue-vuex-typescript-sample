1cc7a6fd0e6eb4c482093e5f6fd8e114704fa35f
Yamatomo <mazda13bturbo@gmail.com>
2019-07-28 12:42:04 +0900

    refactor: setup function

--- stories/components/todo/item.story.ts
+++ stories/components/todo/item.story.ts
@@ -1,54 +1,43 @@
 import { storiesOf } from '@storybook/vue';
+import { VueConstructor } from 'vue';
 import * as Helper from '../../helper';
 import Item from '@/components/todo/item.vue';
 import { StoreHelper } from '@/mixins/store_helper';
 import * as Mapper from '@/store_modules/module_mapper';
 import { TodoState } from '@/store_modules/todo';
 
-const setup = (state: TodoState) => {
+const setup = (
+  initState: TodoState,
+  extendCtx?: NonNullable<ConstructorParameters<VueConstructor>[0]>
+) => {
   const store = Helper.store();
-  store.commit('todo/setInitialState', state);
+  store.commit('todo/setInitialState', initState);
 
-  return [
-    store,
-    StoreHelper.getActions(store, Mapper.modules.todo),
-    StoreHelper.getState(store, Mapper.modules.todo),
-  ] as const;
+  const actions = StoreHelper.getActions(store, Mapper.modules.todo);
+  const state = StoreHelper.getState(store, Mapper.modules.todo);
+
+  return () => ({
+    ...{
+      components: { Item },
+      template: '<Item :actions=actions :todo=todo />',
+      store,
+      data: () => ({ todo: state.todos[0], actions }),
+      i18n: Helper.i18n(),
+    },
+    ...(extendCtx || {}),
+  });
 };
 
 storiesOf('components.todo.item', module)
-  .add('default', () => {
-    const [store, actions, state] = setup({
-      todos: [{ text: 'aaaa', done: true }],
-    });
-
-    return {
-      components: { Item },
-      template: '<Item :actions=actions :todo=todo />',
-      store,
-      data: () => ({
-        todo: state.todos[0],
-        actions,
-      }),
-      i18n: Helper.i18n(),
-    };
-  })
-  .add('edit mode', () => {
-    const [store, actions, state] = setup({
-      todos: [{ text: 'bbbb', done: false }],
-    });
-
-    return {
-      components: { Item },
-      template: '<Item :actions=actions :todo=todo />',
-      store,
-      data: () => ({
-        todo: state.todos[0],
-        actions,
-      }),
-      mounted() {
-        (this as any).$children[0].editing = 1;
-      },
-      i18n: Helper.i18n(),
-    };
-  });
+  .add('default', setup({ todos: [{ text: 'aaaa', done: true }] }))
+  .add(
+    'edit mode',
+    setup(
+      { todos: [{ text: 'bbbb', done: false }] },
+      {
+        mounted() {
+          (this as any).$children[0].editing = 1;
+        },
+      }
+    )
+  );
