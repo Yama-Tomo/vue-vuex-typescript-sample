4528d25fa79e64344beba34b358568bf5671a834
Yama-Tomo <mazda13bturbo@gmail.com>
2019-08-14 17:15:52 +0900

    feat: reconsideration lint rules

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,18 +1,23 @@
 // NOTE: how to apply eslint on intellij https://intellij-support.jetbrains.com/hc/en-us/community/posts/115000225170-ESLint-and-ts-Typescript-files
 module.exports = {
   root: true,
-  plugins: ['@typescript-eslint', 'babel', 'prettier', 'standard'],
+  plugins: ['babel', 'prettier'],
+  // parser: 'vue-eslint-parser', # This configuration is not necessary. @nuxtjs/eslint-config-typescript => @nuxtjs/eslint-config => eslint-plugin-vue
   parserOptions: {
     parser: '@typescript-eslint/parser',
     sourceType: 'module',
+// relate issues: https://github.com/typescript-eslint/typescript-eslint/issues/404, https://github.com/mysticatea/vue-eslint-parser/issues/45
+//    project: './tsconfig.json',
+//    extraFileExtensions: ['.vue']
   },
   rules: {
     semi: 'off',
     'babel/semi': 'error',
+    '@typescript-eslint/no-explicit-any': 'off',
+    '@typescript-eslint/explicit-function-return-type': 'off',
+    '@typescript-eslint/camelcase': ['error', { properties: 'never' }],
+    "@typescript-eslint/no-empty-interface": ["error", { allowSingleExtends: true } ],
     'comma-dangle': ['error', 'always-multiline'],
-    '@typescript-eslint/no-unused-vars': 'error',
-    '@typescript-eslint/member-delimiter-style': 'error',
-    '@typescript-eslint/explicit-member-accessibility': 'error',
     'prettier/prettier': [
       'error',
       {
@@ -22,8 +27,13 @@ module.exports = {
     ]
   },
   extends: [
-    '@nuxtjs',
+    'eslint:recommended',
+    'plugin:@typescript-eslint/eslint-recommended',
+    'plugin:@typescript-eslint/recommended',
+//    'plugin:@typescript-eslint/recommended-requiring-type-checking',
+    '@nuxtjs/eslint-config-typescript',
     'plugin:prettier/recommended',
+    'prettier/@typescript-eslint',
     'prettier',
     'prettier/babel',
     'prettier/vue',
