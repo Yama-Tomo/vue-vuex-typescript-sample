bcc37f64cb9f371e54b6a42ad0eec9276776f8ba
Yamatomo <mazda13bturbo@gmail.com>
2019-08-15 12:52:29 +0900

    refactor: give permission paths from environment variable

--- docker/ruby/entrypoint.sh
+++ docker/ruby/entrypoint.sh
@@ -5,6 +5,12 @@ GROUP_ID=${LOCAL_GID:-0}
 
 echo "Starting with UID : $USER_ID, GID: $GROUP_ID"
 adduser -u $USER_ID -g $GROUP_ID -s /bin/sh -D user
-chown -R user. /usr/local/bundle
+
+if [ "`id user`" != "" ]; then
+  for path in $CHANGE_PERMISSION_PATHS
+  do
+    [ -e $path ] && chown -R user. $path
+  done
+fi
 
 exec su-exec user "$@"
