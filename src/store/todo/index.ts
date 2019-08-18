664100c530721a2717e0cca7734c6659a87f7d34
Yama-Tomo <mazda13bturbo@gmail.com>
2019-08-18 20:48:17 +0900

    example: add submodule of Todo module

--- src/store/todo/index.ts
+++ src/store/todo/index.ts
@@ -3,21 +3,28 @@ import actions from '@/store/todo/actions';
 import mutations from '@/store/todo/mutations';
 import getters from '@/store/todo/getters';
 import * as Store from '@/types/store';
+import * as Sub1 from '@/store/todo/sub1';
 
 export * from '@/store/todo/state';
 
 export { state, actions, mutations, getters };
 
 export type StateTree = {
-  todo: State;
-};
+  todo: State & {
+    sub1: Sub1.StateTree;
+  };
+} & Sub1.StateTree;
 
 export type RootState = Pick<StateTree, 'todo'>;
 
 export type GetterTree = {
-  todo: Store.GetterReturnType<typeof getters>;
-};
+  todo: Store.GetterReturnType<typeof getters> & {
+    sub1: Sub1.GetterTree;
+  };
+} & Sub1.GetterTree;
 
 export type ActionTree = {
-  todo: Store.DispatchArgs<typeof actions>;
-};
+  todo: Store.DispatchArgs<typeof actions> & {
+    sub1: Sub1.ActionTree;
+  };
+} & Sub1.ActionTree;
