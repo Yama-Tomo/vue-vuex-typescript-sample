0bc9fc18779e2d72757a0d1215d93c5dfb79dab2
Yama-Tomo <mazda13bturbo@gmail.com>
2019-08-19 14:52:23 +0900

    refactor: setup vuex store

--- src/store.ts
+++ src/store.ts
@@ -1,20 +1,13 @@
 import Vue from 'vue';
 import Vuex from 'vuex';
-import { modules } from './modules/module_mapper';
-import todoStore from './modules/todo/store';
+import { module as TodoModule } from '@/modules/todo/store';
 
 Vue.use(Vuex);
 
-export default (moduleName: string, initialState?: any) => {
-  const storeModules = (() => {
-    if (moduleName === modules.todo) {
-      return { [modules.todo]: todoStore(initialState) };
-    }
-
-    throw new Error('invalid module name');
-  })();
-
+export default (initialState?: any) => {
   return new Vuex.Store({
-    modules: storeModules,
+    modules: {
+      ...TodoModule(initialState),
+    },
   });
 };
