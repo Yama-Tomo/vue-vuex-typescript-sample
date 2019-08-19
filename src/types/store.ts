ba9987e136bd450ce65efac626ebcf41326a1cec
Yama-Tomo <mazda13bturbo@gmail.com>
2019-08-19 14:48:25 +0900

    feat: add type definition helpers of Vuex Store

---
+++ src/types/store.ts
@@ -0,0 +1,29 @@
+import { DefineActions } from 'vuex-type-helper';
+
+type Functions = Record<string, (...arg: any[]) => void>;
+
+export type MutationArgs<M extends Functions> = {
+  [P in keyof M]: Parameters<(M)[P]>[1]
+};
+
+// prettier-ignore
+type Actions<S, M extends Functions> = DefineActions<Record<string, any>, S, MutationArgs<M>>;
+// prettier-ignore
+type _ActionContext<S, M extends Functions> = Parameters<Actions<S, M>[string]>[0];
+
+export type ActionContext<S, M extends Functions, RootState> = {
+  [P in keyof _ActionContext<S, M>]: 'rootState' extends P
+    ? RootState
+    : _ActionContext<S, M>[P]
+};
+
+export type DispatchArgs<A extends Functions> = {
+  [P in keyof A]: Parameters<(A)[P]>[1] extends undefined
+    ? () => Promise<any> | void
+    : (payload: Parameters<(A)[P]>[1]) => Promise<any> | void
+};
+
+// prettier-ignore
+export type GetterReturnType<G extends Record<string, (...arg: any[]) => any>> = {
+  [P in keyof G]: ReturnType<G[P]>;
+};
