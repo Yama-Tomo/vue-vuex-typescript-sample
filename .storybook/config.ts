17d9e89ce66586421199545f41e79d69a999a750
Yama-Tomo <mazda13bturbo@gmail.com>
2019-08-22 16:29:42 +0900

    chore: remove unnecessary import

--- .storybook/config.ts
+++ .storybook/config.ts
@@ -1,16 +1,12 @@
 import { configure } from '@storybook/vue';
 import Vue from 'vue';
-import Vuex from 'vuex';
 import VueI18n from 'vue-i18n';
-import Router from 'vue-router';
 
 if (process.env.NODE_ENV === 'test') {
   require('babel-plugin-require-context-hook/register')();
 }
 
-Vue.use(Vuex);
 Vue.use(VueI18n);
-Vue.use(Router);
 
 function loadStories() {
   const req = require.context('../stories', true, /.story.tsx?$/);
