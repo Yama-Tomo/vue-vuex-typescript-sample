dc761bd6597fbd04cc235003c77f4097d90e850d
Yama-Tomo <mazda13bturbo@gmail.com>
2019-08-22 17:02:33 +0900

    fix(lint): ignore redeclare rule

--- src/types/storybook-nuxt-integration.d.ts
+++ src/types/storybook-nuxt-integration.d.ts
@@ -1,6 +1,7 @@
 import { Store } from 'vuex';
 import VueRouter from 'vue-router';
 
+/* eslint no-redeclare: 0 */
 declare global {
   const StoryBookNuxtIntegrationVuex: {
     createStore: () => Store<any>;
