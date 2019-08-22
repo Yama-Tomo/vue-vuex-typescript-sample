89cbe889c2cde1286e54858212828fcdccd109f2
Yama-Tomo <mazda13bturbo@gmail.com>
2019-08-22 16:17:53 +0900

    feat: put builded nuxt file into the `webpack provide plugin` and use in storybook

--- stories/helper.ts
+++ stories/helper.ts
@@ -1,21 +1,11 @@
-import Vuex from 'vuex';
+import { Store } from 'vuex';
 import VueI18n from 'vue-i18n';
 import VueRouter from 'vue-router';
 import { state, actions, mutations, getters } from '@/store/todo';
 import i18nEnMessage from '@/i18n/en';
 
-export const store = () =>
-  new Vuex.Store({
-    modules: {
-      todo: {
-        namespaced: true,
-        state,
-        actions,
-        mutations,
-        getters,
-      },
-    },
-  });
+export const store = (): Store<any> =>
+  StoryBookNuxtIntegrationVuex.createStore();
 
 export const i18n = () =>
   new VueI18n({
@@ -23,4 +13,4 @@ export const i18n = () =>
     messages: { en: i18nEnMessage },
   });
 
-export const router = () => new VueRouter({ routes: [] });
+export const router = () => StoryBookNuxtIntegrationRouter.createRouter();
