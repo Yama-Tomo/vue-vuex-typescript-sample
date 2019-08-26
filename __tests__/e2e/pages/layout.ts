1adad93ea28132ad1a8c01036652f3f2851b46ed
Yama-Tomo <mazda13bturbo@gmail.com>
2019-08-26 18:11:08 +0900

    test: update tests

---
+++ __tests__/e2e/pages/layout.ts
@@ -0,0 +1,71 @@
+import { Browser, Page } from 'puppeteer';
+
+export default class Layout {
+  private readonly page: Page;
+  private readonly browser: Browser;
+
+  public constructor(page: Page, browser: Browser) {
+    this.page = page;
+    this.browser = browser;
+  }
+
+  public async gotoLogin(cb?: () => void) {
+    await this.page.click(
+      '#app > div.v-application--wrap > header > div > div.v-toolbar__items > button'
+    );
+    await this.page.waitForSelector(
+      '.v-menu__content--fixed.menuable__content__active',
+      { visible: true }
+    );
+    await this.page.click(
+      '.v-menu__content--fixed.menuable__content__active > div > a:last-child'
+    );
+
+    if (cb) {
+      await cb();
+    }
+  }
+
+  public async doLogout(cb?: () => void) {
+    await this.page.click(
+      '#app > div.v-application--wrap > header > div > div.v-toolbar__items > button'
+    );
+    await this.page.waitForSelector(
+      '.v-menu__content--fixed.menuable__content__active',
+      { visible: true }
+    );
+    await this.page.click(
+      '.v-menu__content--fixed.menuable__content__active > div > a:last-child'
+    );
+
+    if (cb) {
+      await cb();
+    }
+  }
+
+  public async gotoOtherLangThisPage(cb?: () => void) {
+    const currentUrl = await this.page.url();
+
+    await this.page.click(
+      '#app > div.v-application--wrap > header > div > button'
+    );
+    await this.page.waitForSelector(
+      '.v-menu__content--fixed.menuable__content__active',
+      { visible: true }
+    );
+
+    await this.page.click(
+      '.v-menu__content--fixed.menuable__content__active > div > a:not(.v-list-item--active)'
+    );
+
+    await this.page.waitForFunction(
+      c => window.location.href !== c,
+      { polling: 'raf' },
+      currentUrl
+    );
+
+    if (cb) {
+      await cb();
+    }
+  }
+}
--- __tests__/e2e/pages/top.ts
+++ __tests__/e2e/pages/top.ts
@@ -2,14 +2,17 @@ import { Browser, ElementHandle, Page } from 'puppeteer';
 import { deleteAll } from '../utils/keyboard';
 import { getElement } from '../utils/element';
 import Login from './login';
+import Layout from './layout';
 
 export default class Top {
   private readonly page: Page;
   private readonly browser: Browser;
+  private readonly layout: Layout;
 
   public constructor(page: Page, browser: Browser) {
     this.page = page;
     this.browser = browser;
+    this.layout = new Layout(page, browser);
   }
 
   public async open(cb?: () => void) {
@@ -23,39 +26,27 @@ export default class Top {
   }
 
   public async assertThisPageVisited() {
-    await this.page.waitForSelector(
-      '#__layout > div > div:nth-child(3) > section > header > h1',
-      { visible: true }
-    );
+    await this.page.waitForSelector('main header > h1', { visible: true });
   }
 
-  public async gotoLogin(cb?: () => void) {
-    await this.page.click('#__layout > div > div.signin > a');
-    if (!cb) {
-      const loginPage = new Login(page, browser);
-      await loginPage.assertThisPageVisited();
-    } else {
-      await cb();
-    }
+  public async gotoLogin() {
+    const loginPage = new Login(page, browser);
+    await this.layout.gotoLogin(() => loginPage.assertThisPageVisited());
   }
 
   public async inputTodo(todoContent: string) {
-    // eslint-disable-next-line quotes
-    await this.page.type(
-      '#__layout > div > div:nth-child(3) > section > header > input',
-      todoContent + '\n'
-    );
+    await this.page.type('main header input', todoContent + '\n');
   }
 
   public async editTodo(
     todoContent: string,
     liElement: ElementHandle<Element>
   ) {
-    await getElement(liElement, 'label').then(e => e.click({ clickCount: 2 }));
+    await getElement(liElement, '.text').then(e => e.click());
     await page.waitFor(100);
 
     const inputEle = await (() => {
-      return liElement.$('input.edit').then(e => {
+      return liElement.$('.v-text-field input').then(e => {
         if (e === null) throw new Error('not found input element');
 
         return e;
@@ -63,19 +54,16 @@ export default class Top {
     })();
 
     await deleteAll(inputEle);
-    // eslint-disable-next-line quotes
     await inputEle.type(todoContent + '\n');
     await page.waitFor(100);
   }
 
   public getTodoElements() {
-    return this.page.$$(
-      '#__layout > div > div:nth-child(3) > section > section > ul > li'
-    );
+    return this.page.$$('main .v-list > div');
   }
 
   public getTodoContent(liElement: ElementHandle<Element>) {
-    return getElement(liElement, 'label').then(e =>
+    return getElement(liElement, '.text').then(e =>
       page.evaluate(_e => (_e as Element).textContent, e)
     );
   }
@@ -89,45 +77,24 @@ export default class Top {
   }
 
   public getActiveTodoElements() {
-    return this.page.$$(
-      "#__layout > div > div:nth-child(3) > section > section > ul > li input[type='checkbox']:checked"
-    );
+    return this.page.$$("main .v-list > div input[type='checkbox']:checked");
   }
 
   public getCompletedTodoElements() {
     return this.page.$$(
-      "#__layout > div > div:nth-child(3) > section > section > ul > li input[type='checkbox']:not(:checked)"
+      "main .v-list > div input[type='checkbox']:not(:checked)"
     );
   }
 
   public filterActiveTodo() {
-    return this.page.click(
-      '#__layout > div > div:nth-child(3) > section > footer > ul > li:nth-child(2) > a'
-    );
+    return this.page.click('main a.v-tab:nth-of-type(2)');
   }
 
   public filterCompletedTodo() {
-    return this.page.click(
-      '#__layout > div > div:nth-child(3) > section > footer > ul > li:nth-child(3) > a'
-    );
+    return this.page.click('main a.v-tab:nth-of-type(3)');
   }
 
-  public async gotoOtherLangThisPage(cb?: () => void) {
-    const currentUrl = await this.page.url();
-
-    await this.page.click(
-      '#__layout > div > div:nth-child(5) > a:nth-child(4)'
-    );
-    await this.page.waitForFunction(
-      c => window.location.href !== c,
-      { polling: 'raf' },
-      currentUrl
-    );
-
-    if (!cb) {
-      await this.assertThisPageVisited();
-    } else {
-      await cb();
-    }
+  public async gotoOtherLangThisPage() {
+    await this.layout.gotoOtherLangThisPage(() => this.assertThisPageVisited());
   }
 }
