655e4a78e85e37485a6fccdc4963c3250c69b95c
Yama-Tomo <mazda13bturbo@gmail.com>
2020-01-26 01:20:45 +0900

    chore: add wait timeout option

--- docker/puppeteer/wait_for_backend.sh
+++ docker/puppeteer/wait_for_backend.sh
@@ -1,6 +1,14 @@
 #!/bin/sh
 
+limit=${2:-60}
+count=0
+
 until wget -O - $1 >/dev/null 2>&1; do
   >&2 echo "Wait for backend API server [$1]"
   sleep 1
+  count=`expr $count + 1`
+
+  if [ $count -eq $limit ]; then
+    exit 1
+  fi    
 done
