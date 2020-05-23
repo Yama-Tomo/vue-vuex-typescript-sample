387079fa3651b18c34c8d943b1ec69385416efee
Yama-Tomo <mazda13bturbo@gmail.com>
2020-05-23 14:28:53 +0900

    refactor: vuex setup

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,21 @@
 import Vue from 'vue';
+import Vuex from 'vuex';
 import App from './App.vue';
-import store from './store';
+import { TodoModule } from './modules/store';
 
 Vue.config.productionTip = false;
+Vue.use(Vuex);
+
+// eslint-disable-next-line @typescript-eslint/explicit-module-boundary-types
+const vuexStore = (initialState?: any) => {
+  return new Vuex.Store({
+    modules: {
+      ...TodoModule(initialState),
+    },
+  });
+};
 
 new Vue({
-  store: store((window as any).__INITIAL_STATE__),
+  store: vuexStore((window as any).__INITIAL_STATE__),
   render: h => h(App),
 }).$mount('#app');
--- src/modules/module_mapper.ts
+++ src/modules/module_mapper.ts
@@ -1,5 +1,7 @@
 import * as Todo from './todo/store';
 
+export { module as TodoModule } from '@/modules/todo/store';
+
 export type RootState = Todo.StateTree;
 
 export type GetterTree = Todo.GetterTree;
